9e929c61fde7de08b6257d56466dfef73c3320fb
Oscar <gogleoscar14@gmail.com>
2021-08-18 23:08:55 -0500

    otras lecciones
    display

--- src/main/java/com/oslim/bootstrap/app/controller/HomeController.java
+++ src/main/java/com/oslim/bootstrap/app/controller/HomeController.java
@@ -7,24 +7,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-	
-	@GetMapping("/")
-	public String home() {
-		return "alineaciones";
-	}
-	
-	@GetMapping("/row")
-	public String pagerow() {
-		return "row-columns";
-	}
 
-	@GetMapping("/ani")
-	public String anidacion () {
-		return "anida";
-	}
-	
-	@GetMapping("/ver")
-	public String ver () {
-		return "intentpage/princi";
-	}
+    @GetMapping("/")
+    public String home() {
+        return "alineaciones";
+    }
+
+    @GetMapping("/row")
+    public String pagerow() {
+        return "row-columns";
+    }
+
+    @GetMapping("/ani")
+    public String anidacion() {
+        return "anida";
+    }
+
+    @GetMapping("/ver")
+    public String ver() {
+        return "intentpage/princi";
+    }
+
+    @GetMapping("/display")
+    public String disp() {
+        return "display";
+    }
 }
